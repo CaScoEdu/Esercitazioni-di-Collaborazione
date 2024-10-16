ad3c2bf931d79c0080916a3e6b097d25792b8329
Carlo Scocco <carlo.scocco@einsteinvimercate.edu.it>
2024-10-16 19:10:41 +0200

    aggiunto commento

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,6 +1,6 @@
 public class Buffer {
 
-    public Messaggio messaggio;
+    public Messaggio messaggio; //
 
     public final SemaforoMutex PRELEVATO = new SemaforoMutex(1); // via libera al produttore
     public final SemaforoMutex DEPOSITATO = new SemaforoMutex(0); // blocca il consumatore
